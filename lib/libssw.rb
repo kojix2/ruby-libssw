e5476512ba0933de55ff79caedbfa4d248e0f14c
kojix2 <2xijok@gmail.com>
2021-01-10 02:45:32 +0900

    Path of the shared library

--- lib/libssw.rb
+++ lib/libssw.rb
@@ -8,4 +8,21 @@ module LibSSW
   class << self
     attr_accessor :ffi_lib
   end
+
+  lib_name = case RbConfig::CONFIG['host_os']
+             when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+               'libssw.dll'   # unconfirmed
+             when /darwin|mac os/
+               'libssw.dylib' # unconfirmed
+             else
+               'libssw.so'
+             end
+
+  self.ffi_lib = if ENV['LIBSSWDIR'] && !ENV['LIBSSWDIR'].empty?
+                   File.expand_path(lib_name, ENV['LIBUIDIR'])
+                 else
+                   File.expand_path("../vendor/#{lib_name}", __dir__)
+                 end
+
+  autoload :FFI, 'libssw/ffi'
 end
