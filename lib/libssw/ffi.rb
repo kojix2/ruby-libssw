45a43d5176b3a3ca377cc0ad26f6b327c3834eec
kojix2 <2xijok@gmail.com>
2021-01-10 02:45:40 +0900

    Add some structs

--- lib/libssw/ffi.rb
+++ lib/libssw/ffi.rb
@@ -23,5 +23,27 @@ module LibSSW
         @ffi_methods ||= func_map.each_key.to_a
       end
     end
+
+    Align = struct [
+      'uint16_t score1',
+      'uint16_t score2',
+      'int32_t ref_begin1',
+      'int32_t ref_end1',
+      'int32_t	read_begin1',
+      'int32_t read_end1',
+      'int32_t ref_end2',
+      'uint32_t* cigar',
+      'int32_t cigarLen'
+    ]
+
+    Profile = struct [
+      'int32_t* byte', # __m128i* profile_byte;	// 0: none
+      'int32_t* word', # __m128i* profile_word;	// 0: none
+      'const int8_t* read',
+      'const int8_t* mat',
+      'int32_t readLen',
+      'int32_t n',
+      'uint8_t bias'
+    ]
   end
 end
