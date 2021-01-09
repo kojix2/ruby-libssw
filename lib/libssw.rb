99ce7dd062ce9853c5ef72f32d7abae1c0ad354c
kojix2 <2xijok@gmail.com>
2021-01-09 23:15:49 +0900

    Add skeleton

---
+++ lib/libssw.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'libssw/version'
+
+module LibSSW
+  VERSION = "0.0.0"
+  class Error < StandardError; end
+  end
+end
---
+++ lib/libssw/version.rb
@@ -0,0 +1,3 @@
+module LibSSW
+  VERSION = "0.0.0".freeze
+end
