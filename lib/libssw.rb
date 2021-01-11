37f1a91f3324f231c91d142f44fe7f77a4c4226b
kojix2 <2xijok@gmail.com>
2021-01-11 17:53:47 +0900

    Add documents to methods * Official documentation of libssw (partially modified for Ruby)

--- lib/libssw.rb
+++ lib/libssw.rb
@@ -123,6 +123,16 @@ module LibSSW
   end
 
   class << self
+    # Create the query profile using the query sequence.
+    # @param read [Array] query sequence; the query sequence needs to be numbers
+    # @param read_len [Integer] length of the query sequence
+    # @param mat [Array] substitution matrix; mat needs to be corresponding to the read sequence
+    # @param n [Integer] the square root of the number of elements in mat (mat has n*n elements)
+    # @param score_size [Integer]
+    #   estimated Smith-Waterman score;
+    #   * if your estimated best alignment score is surely < 255 please set 0;
+    #   * if your estimated best alignment score >= 255, please set 1;
+    #   * if you don't know, please set 2
     def ssw_init(read, read_len, mat, n, score_size)
       ptr = FFI.ssw_init(
         read.pack('c*'), read_len, mat.flatten.pack('c*'), n, score_size
@@ -130,10 +140,57 @@ module LibSSW
       LibSSW::Profile.new(ptr)
     end
 
+    # Release the memory allocated by function ssw_init.
+    # @param p [Fiddle::Pointer, LibSSW::Profile, LibSSW::FFI::Profile]
+    #   pointer to the query profile structure
     def init_destroy(profile)
       FFI.init_destroy(profile)
     end
 
+    # Do Striped Smith-Waterman alignment.
+    # @param prof [Fiddle::Pointer, LibSSW::Profile, LibSSW::FFI::Profile]
+    #   pointer to the query profile structure
+    # @param ref [Array]
+    #   target sequence;
+    #   the target sequence needs to be numbers and corresponding to the mat 
+    #   parameter of function ssw_init
+    # @param ref_len [Integer] length of the target sequence
+    # @param weight_gap0 [Integer] the absolute value of gap open penalty
+    # @param weight_gapE [Integer] the absolute value of gap extension penalty
+    # @param flag [Integer]
+    #   * bit 5: when setted as 1, function ssw_align will return the best 
+    #     alignment beginning position;
+    #   * bit 6: when setted as 1, if (ref_end1 - ref_begin1 < filterd && 
+    #     read_end1 - read_begin1 < filterd), (whatever bit 5 is setted) the 
+    #     function will return the best alignment beginning position and cigar; 
+    #   * bit 7: when setted as 1, if the best alignment score >= filters, 
+    #     (whatever bit 5 is setted) the function   will return the best 
+    #     alignment beginning position and cigar;
+    #   * bit 8: when setted as 1, (whatever bit 5, 6 or 7 is  setted) the 
+    #     function will always return the best alignment beginning position and
+    #     cigar. When flag == 0, only the optimal and sub-optimal scores and the
+    #     optimal alignment ending position will be returned.
+    # @param filters [Integer]
+    #   scorefilter: when bit 7 of flag is setted as 1 and bit 8 is setted as 0,
+    #   filters will be used (Please check the decription of the flag parameter
+    #   for detailed usage.)
+    # @param filterd [Integer]
+    #   distance filter: when bit 6 of flag is setted as 1 and bit 8 is setted
+    #   as 0, filterd will be used (Please check the decription of the flag
+    #   parameter for detailed usage.)
+    # @param mask_len [Integer]
+    #   The distance between the optimal and suboptimal alignment ending 
+    #   position >= maskLen. We suggest to use readLen/2, if you don't have
+    #   special concerns. Note: maskLen has to be >= 15, otherwise this function
+    #   will NOT return the suboptimal alignment information. Detailed
+    #   description of maskLen: After locating the optimal alignment ending
+    #   position, the suboptimal alignment score can be heuristically found by
+    #   checking the second largest score in the array that contains the maximal
+    #   score of each column of the SW matrix. In order to avoid picking the
+    #   scores that belong to the alignments sharing the partial best alignment,
+    #   SSW C library masks the	reference loci nearby (mask length = maskLen)
+    #   the best alignment ending position and locates the second largest score
+    #   from the unmasked elements.
     def ssw_align(prof, ref, ref_len, weight_gap0, weight_gapE, flag, filters, filterd, mask_len)
       ptr = FFI.ssw_align(
         prof, ref.pack('c*'), ref_len, weight_gap0, weight_gapE, flag, filters, filterd, mask_len
@@ -141,10 +198,35 @@ module LibSSW
       LibSSW::Align.new(ptr)
     end
 
+    # Release the memory allocated by function ssw_align.
+    # @param a [Fiddle::Pointer, LibSSW::Align, LibSSW::FFI::Align]
+    #   pointer to the alignment result structure
     def align_destroy(align)
       FFI.align_destroy(align)
     end
 
+    # 1. Calculate the number of mismatches.
+    # 2. Modify the cigar string:
+    # differentiate matches (=), mismatches(X), and softclip(S).
+    # @note This method takes a Fiddle::Pointer as an argument. Please read the
+    #   source code and understand it well before using this method. 
+    #   (Needs to be improved)
+    # @param	ref_begin1 [Integer]
+    #   0-based best alignment beginning position on the reference sequence
+    # @param	read_begin1 [Integer]
+    #   0-based best alignment beginning position on the read sequence
+    # @param	read_end1 [Integer]
+    #   0-based best alignment ending position on the read sequence
+    # @param	ref [Array]
+    #   reference sequence
+    # @param	read [Array]
+    #   read sequence
+    # @param	read_len [Integer] length of the read
+    # @param	cigar [Fiddle::Pointer] 
+    #   best alignment cigar; stored the same as that in BAM format,
+    #   high 28 bits: length, low 4 bits: M/I/D (0/1/2)
+    # @param	cigar_len [Integer] length of the cigar string
+    # @return [Integer] The number of mismatches. The cigar and cigarLen are modified.
     def mark_mismatch(ref_begin1, read_begin1, read_end1, ref, read, read_len, cigar, cigar_len)
       warn 'implementation: fiexme: **cigar' # FIXME
       FFI.mark_mismatch(
