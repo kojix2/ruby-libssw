8aa80e88d41c58ec228519f197cc4487f2f3bfc7
kojix2 <2xijok@gmail.com>
2021-01-11 14:21:00 +0900

    Add simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'simplecov'
+SimpleCov.start
+require 'bundler/setup'
+Bundler.require(:default)
 require 'libssw'
 
 require 'minitest/autorun'
