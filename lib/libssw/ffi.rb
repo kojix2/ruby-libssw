174da8487bbab208031218a32b2640ea5415c1ad
kojix2 <2xijok@gmail.com>
2021-01-13 01:25:31 +0900

    Remove needless code

--- lib/libssw/ffi.rb
+++ lib/libssw/ffi.rb
@@ -13,16 +13,10 @@ module LibSSW
     end
 
     class << self
-      attr_reader :func_map
-
       def try_extern(signature, *opts)
         extern(signature, *opts)
       rescue StandardError => e
         warn "#{e.class.name}: #{e.message}"
-      end
-
-      def ffi_methods
-        @ffi_methods ||= func_map.each_key.to_a
       end
     end
 
